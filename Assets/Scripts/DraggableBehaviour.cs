da2772d4af9b11ef991f942606e629123e354e8b
Bdiebeak <123redcreeper123@gmail.com>
2020-09-20 22:39:42 +0400

    Добавил скрипт реализации перетаскивания
    Так же создал отдельные настройки для перетаскивания

---
+++ Assets/Scripts/DraggableBehaviour.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+/// <summary>
+/// Класс, реализующий перетаскивание Rigidbody объектов.
+/// При перетаскивании задается фиксированная высота по координате У,
+/// к которой плавно поднимается объект.
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class DraggableBehaviour : MonoBehaviour
+{
+    [Header("\tНастройки")] 
+    [SerializeField]
+    private DraggableBehaviourSettings settings = null;
+
+    private float _risingStep = 0;
+
+    private Camera _mainCamera;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        if (settings == null)
+        {
+            Debug.LogError($"{gameObject.name} не назначены настройки.");
+        }
+        
+        _mainCamera = Camera.main;
+        _rigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        TurnOnKinematic();
+    }
+
+    private void OnMouseDrag()
+    {
+        var newPosition = CalculateNewObjectPosition();
+        
+        transform.position = Vector3.Lerp(transform.position, newPosition, _risingStep);
+        IncreaseStep();
+    }
+
+    private void OnMouseUp()
+    {
+        TurnOffKinematic();
+        ResetStep();
+    }
+
+    private void TurnOnKinematic()
+    {
+        _rigidbody.isKinematic = true;
+    }
+    
+    private Vector3 CalculateNewObjectPosition()
+    {
+        var objectScreenPositionZ = _mainCamera.WorldToScreenPoint(transform.position).z;
+        var newScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objectScreenPositionZ);
+        var newWorldPosition = _mainCamera.ScreenToWorldPoint(newScreenPosition);
+        newWorldPosition.y = settings.yDraggingValue;
+        
+        return newWorldPosition;
+    }
+
+    private void IncreaseStep()
+    {
+        _risingStep += settings.risingSpeed * Time.deltaTime;
+    }
+
+    private void TurnOffKinematic()
+    {
+        _rigidbody.isKinematic = false;
+    }
+
+    private void ResetStep()
+    {
+        _risingStep = 0;
+    }
+}
---
+++ Assets/Scripts/DraggableBehaviourSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DB Settings", menuName = "Draggable Behaviour Settings")]
+public class DraggableBehaviourSettings : ScriptableObject
+{
+    public float yDraggingValue = 1.5f;
+    public float risingSpeed = 0.3f;
+}
