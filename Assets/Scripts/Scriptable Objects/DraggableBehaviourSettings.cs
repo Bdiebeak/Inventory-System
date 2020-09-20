4e049c6c9e120d8667cc512f10b715a5b1d6047c
Bdiebeak <123redcreeper123@gmail.com>
2020-09-21 00:42:48 +0400

    Переместил скрипты SO в отдельную папку

--- Assets/Scripts/Scriptable Objects/DraggableBehaviourSettings.cs
+++ Assets/Scripts/Scriptable Objects/DraggableBehaviourSettings.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DB Settings", menuName = "Draggable Behaviour Settings")]
 public class DraggableBehaviourSettings : ScriptableObject
 {
-    public float yDraggingValue = 1.5f;
+    public float yDraggingHeight = 1.5f;
     public float risingSpeed = 0.3f;
 }
