9503b06d0fa6581ad0062ccf334ec004398e74de
Bdiebeak <123redcreeper123@gmail.com>
2020-09-21 00:45:08 +0400

    Скрипт управления инвентарем
    Пока что намешано, потом буду рефакторить

---
+++ Assets/Scripts/Backpack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Backpack : MonoBehaviour
+{
+    public GameObject UIgo;
+    public Image AccessUIgo;
+    public float timeToAccess;
+    
+    private bool _accessEnabled;
+    private float _currentTime;
+    private float _requiredTime;
+
+    private void OnMouseDown()
+    {
+        InitializeAccessTimer();
+    }
+
+    private void OnMouseDrag()
+    {
+        if (_accessEnabled)
+        {
+            ShowBackpackUI();
+        }
+        else
+        {
+            if (_currentTime < _requiredTime)
+            {
+                _currentTime += Time.deltaTime;
+                AccessUIgo.fillAmount = 1 - (_requiredTime - _currentTime) / timeToAccess;
+            }
+            else
+            {
+                _accessEnabled = true;
+                AccessUIgo.fillAmount = 1f;
+            }
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        HideBackpackUI();
+        ResetAccess();
+        AccessUIgo.fillAmount = 0f;
+    }
+
+    private void ShowBackpackUI()
+    {
+        UIgo.SetActive(true);
+    }
+    
+    private void HideBackpackUI()
+    {
+        UIgo.SetActive(false);
+    }
+    
+    private void InitializeAccessTimer()
+    {
+        _currentTime = Time.time;
+        _requiredTime = Time.time + timeToAccess;
+    }
+
+    private void ResetAccess()
+    {
+        _currentTime = 0;
+        _accessEnabled = false;
+    }
+}
