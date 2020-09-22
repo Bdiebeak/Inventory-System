a629e5eb32cba8add03a1917e095b9d1bf7d61c1
Bdiebeak <123redcreeper123@gmail.com>
2020-09-22 14:59:39 +0400

    Скрипт работы с содержимом рюкзака

--- Assets/Scripts/Backpack/BackpackContents.cs
+++ Assets/Scripts/Backpack/BackpackContents.cs
@@ -1,11 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackpackContents : MonoBehaviour
 {
-    public void Pressed(string message)
+    private Weapon[] _contents;
+    
+    public event Action<Weapon> OnWeaponAdd;
+    public event Action<Weapon> OnWeaponRemove;
+
+    private void Start()
     {
-        Debug.Log(message);
+        _contents = new Weapon[Enum.GetNames(typeof(WeaponType)).Length];
+    }
+
+    public void TryAddWeapon(Weapon weapon)
+    {
+        if (_contents[GetWeaponTypeIndex(weapon)] != null)
+        {
+            // Слот уже занят
+            return;
+        }
+        
+        _contents[GetWeaponTypeIndex(weapon)] = weapon;
+        
+        OnWeaponAdd?.Invoke(weapon);
+    }
+
+    public void TryTakeWeapon(Weapon weapon)
+    {
+        if (_contents[GetWeaponTypeIndex(weapon)] != weapon)
+        {
+            // В слоте другой объект
+            return;
+        }
+
+        _contents[GetWeaponTypeIndex(weapon)] = null;
+     
+        OnWeaponRemove?.Invoke(weapon);
+    }
+
+    private int GetWeaponTypeIndex(Weapon weapon)
+    {
+        return (int) weapon.weaponSettings.type;
     }
 }
