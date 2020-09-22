cb90b7f7d3238a3196ee0aae70eea07abc18c119
Bdiebeak <123redcreeper123@gmail.com>
2020-09-22 21:01:50 +0400

    Скрипт работы содержимого рюкзака
    - Добавил функцию вычисления индекса
    - Размер хранится в отдельной переменной

--- Assets/Scripts/Backpack/BackpackContents.cs
+++ Assets/Scripts/Backpack/BackpackContents.cs
@@ -5,44 +5,54 @@ using UnityEngine;
 
 public class BackpackContents : MonoBehaviour
 {
+    public readonly int contentsSize = Enum.GetNames(typeof(WeaponType)).Length;
+    
     private Weapon[] _contents;
     
     public event Action<Weapon> OnWeaponAdd;
     public event Action<Weapon> OnWeaponRemove;
 
-    private void Start()
+    private void Awake()
     {
-        _contents = new Weapon[Enum.GetNames(typeof(WeaponType)).Length];
+        _contents = new Weapon[contentsSize];
     }
 
     public void TryAddWeapon(Weapon weapon)
     {
-        if (_contents[GetWeaponTypeIndex(weapon)] != null)
+        if (_contents[weapon.GetWeaponTypeIndex()] != null)
         {
             // Слот уже занят
             return;
         }
         
-        _contents[GetWeaponTypeIndex(weapon)] = weapon;
+        _contents[weapon.GetWeaponTypeIndex()] = weapon;
         
         OnWeaponAdd?.Invoke(weapon);
     }
 
     public void TryTakeWeapon(Weapon weapon)
     {
-        if (_contents[GetWeaponTypeIndex(weapon)] != weapon)
+        if (_contents[weapon.GetWeaponTypeIndex()] != weapon)
         {
             // В слоте другой объект
             return;
         }
 
-        _contents[GetWeaponTypeIndex(weapon)] = null;
+        _contents[weapon.GetWeaponTypeIndex()] = null;
      
         OnWeaponRemove?.Invoke(weapon);
     }
 
-    private int GetWeaponTypeIndex(Weapon weapon)
+    public void TakeWeapon(int index)
     {
-        return (int) weapon.weaponSettings.type;
+        if (_contents[index] == null)
+        {
+            // В слоте пусто
+            return;
+        }
+
+        var weapon = _contents[index];
+
+        TryTakeWeapon(weapon);
     }
 }
