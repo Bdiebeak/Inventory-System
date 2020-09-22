79bc7d8d6ddff3f83b8c0d5c30839512b0d7d82e
Bdiebeak <123redcreeper123@gmail.com>
2020-09-22 14:58:23 +0400

    Добавил иконки в SO оружия

---
+++ Assets/Scripts/Weapons/WeaponSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapons/Create New Weapon")]
+public class WeaponSettings : ScriptableObject
+{
+    public new string name;
+    public float weight;
+    public int id;
+    public WeaponType type;
+    public Sprite icon;
+}
