eefe6dfebbffcbb79601c60e18aa96d7f4616d8a
Bdiebeak <123redcreeper123@gmail.com>
2020-09-22 21:02:45 +0400

    Функция вычисления индекса оружия

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -7,11 +7,16 @@ public class Weapon : MonoBehaviour
 {
     public WeaponSettings weaponSettings;
 
-    private void Start()
+    private void Awake()
     {
         if (weaponSettings == null)
         {
             Debug.LogError($"{gameObject.name} не назначены настройки.");
         }
+    }
+
+    public int GetWeaponTypeIndex()
+    {
+        return (int) weaponSettings.type;
     }
 }
